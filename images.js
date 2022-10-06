9efa79e667f5b937505b72c78853b4eee93f252d
kylerobertduncan <kylerobertduncan@gmail.com>
2022-10-06 17:40:29 -0400

    Rebuilding selector

---
+++ images.js
@@ -0,0 +1,50 @@
+export const images = [
+  {
+    altText: "Hills",
+    id: "background1",
+    type: "background",
+    path: "backgrounds/Test_field_1.png"
+  },
+  {
+    altText: "Path",
+    id: "background2",
+    type: "background",
+    path: "backgrounds/Test_field_2.png"
+  },
+  {
+    altText: "Imp",
+    id: "character1",
+    type: "character",
+    path: "bodies/Test_Imp.png"
+  },
+  {
+    altText: "Mouse",
+    id: "character2",
+    type: "character",
+    path: "bodies/Test_Mouse.png"
+  },
+  {
+    altText: "Slime",
+    id: "character3",
+    type: "character",
+    path: "bodies/Test_slime.png"
+  },
+  {
+    altText: "Dress",
+    id: "clothing1",
+    type: "clothing",
+    path: "outfits/Test_Dress.png"
+  },
+  {
+    altText: "Hi Kyle",
+    id: "clothing2",
+    type: "clothing",
+    path: "outfits/Test_Kyle.png"
+  },
+  {
+    altText: "Traveller",
+    id: "clothing3",
+    type: "clothing",
+    path: "outfits/Test_Travelling.png"
+  }
+];
--- script.js
+++ script.js
@@ -1,52 +1,66 @@
-const options = [
-  {
-    id: "background1",
-    type: "background",
-    path: "backgrounds/Test_field_1.png"
-  },
-  {
-    id: "background2",
-    type: "background",
-    path: "backgrounds/Test_field_2.png"
-  },
-  {
-    id: "character1",
-    type: "character",
-    path: "bodies/Test_Imp.png"
-  },
-  {
-    id: "character2",
-    type: "character",
-    path: "bodies/Test_Mouse.png"
-  },
-  {
-    id: "character3",
-    type: "character",
-    path: "bodies/Test_slime.png"
-  },
-  {
-    id: "clothing1",
-    type: "clothing",
-    path: "outfits/Test_Dress.png"
-  },
-  {
-    id: "clothing2",
-    type: "clothing",
-    path: "outfits/Test_Kyle.png"
-  },
-  {
-    id: "clothing3",
-    type: "clothing",
-    path: "outfits/Test_Travelling.png"
-  }
-]
+// const options = [
+//   {
+//     "altText": "Hills",
+//     "id": "background1",
+//     "type": "background",
+//     "path": "backgrounds/Test_field_1.png"
+//   },
+//   {
+//     "altText": "Path",
+//     "id": "background2",
+//     "type": "background",
+//     "path": "backgrounds/Test_field_2.png"
+//   },
+//   {
+//     "altText": "Imp",
+//     "id": "character1",
+//     "type": "character",
+//     "path": "bodies/Test_Imp.png"
+//   },
+//   {
+//     "altText": "Mouse",
+//     "id": "character2",
+//     "type": "character",
+//     "path": "bodies/Test_Mouse.png"
+//   },
+//   {
+//     "altText": "Slime",
+//     "id": "character3",
+//     "type": "character",
+//     "path": "bodies/Test_slime.png"
+//   },
+//   {
+//     "altText": "Dress",
+//     "id": "clothing1",
+//     "type": "clothing",
+//     "path": "outfits/Test_Dress.png"
+//   },
+//   {
+//     "altText": "Hi Kyle",
+//     "id": "clothing2",
+//     "type": "clothing",
+//     "path": "outfits/Test_Kyle.png"
+//   },
+//   {
+//     "altText": "Traveller",
+//     "id": "clothing3",
+//     "type": "clothing",
+//     "path": "outfits/Test_Travelling.png"
+//   }
+// ]
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach(b => {
-  const option = options.filter(o => o.id === b.id);
-  const { path, type } = option[0];
-  const layer = document.querySelector(`.${type}`);
-  b.addEventListener("click", () => {
-    layer.src = `./assets/${path}`;
-  })
-});
+// const buttons = document.querySelectorAll("button");
+// buttons.forEach(b => {
+//   const option = options.filter(o => o.id === b.id);
+//   console.log(option);
+//   const { altText, path, type } = option[0];
+//   const layer = document.querySelector(`.${type}`);
+//   b.addEventListener("click", () => {
+//     layer.src = `./assets/${path}`;
+//     layer.alt = altText;
+//   })
+// });
+
+import { images } from "./images.js";
+
+console.log(images);
